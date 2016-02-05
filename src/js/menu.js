9e2bafb52f7bc853c0a4859f09f7b210a95243ec
Peter Strumolo <Peter Strumolo>
2016-02-05 14:52:46 -0500

    Adds header nav menu with functionality

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,1 +1,8 @@
-console.log("The Menu File.");
+(function() {
+  $(".menu button").click(function() {
+    console.log("click1");
+    $(this).toggleClass("menu-open");
+    $(".menu-slide").toggleClass("menu-open");
+    $("body").toggleClass("menu-open");
+  });
+})();
