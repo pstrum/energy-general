89cf9ede3bca9b624a66b79015a489d1b695ec1c
Peter Strumolo <Peter Strumolo>
2016-02-05 15:13:10 -0500

    Removed excess menu scripting

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,5 @@
 (function() {
   $(".menu button").click(function() {
-    console.log("click1");
     $(this).toggleClass("menu-open");
     $(".menu-slide").toggleClass("menu-open");
     $("body").toggleClass("menu-open");
