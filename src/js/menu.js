b166250ac61422da4249334ac0a83aa15f729ce9
pstrum <pstrum@gmail.com>
2016-03-27 12:03:08 -0400

    Update the layout for larger screens

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,20 @@
 (function() {
-  $(".menu button").click(function() {
-    $(this).toggleClass("menu-open");
-    $(".menu-slide").toggleClass("menu-open");
-    $("body").toggleClass("menu-open");
+  "use strict";
+
+  $(".menu button").click(function(event) {
+
+    event.preventDefault();
+
+    var windowPosition = -Math.abs(window.pageYOffset);
+
+    if ($("body").hasClass("menu-open")) {
+      $("window").scrollTop(500);
+      $("body").removeClass("fixed").removeClass("menu-open").removeAttr("style");
+      $("html").removeClass("body-menu");
+    } else {
+      $("body").css('top', windowPosition).addClass("menu-open").addClass("fixed");
+      $("html").addClass("body-menu");
+    }
   });
 })();
+
